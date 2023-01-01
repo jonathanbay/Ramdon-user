0499497898436067c0a5753afbde2e127f7b5a5a
Jonathn bay <bayart.jonathan@laposte.net>
2023-01-01 16:43:13 +0100

    affichage infos card

--- app.js
+++ app.js
@@ -13,10 +13,32 @@ const fetchUser = async () => {
 const userDisplay =async () => {
     await fetchUser();
 
+    const dateParser = (date) => {
+        let newDate = new Date(date).toLocaleDateString("fr-FR", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+        return newDate;
+    };
+
+    // 
+    const daycalc = (date) => {
+        let today = new Date();
+        let todayTimestamp = Date.parse(today);
+        let timestamp = Date.parse(date)
+
+        return Math.ceil((todayTimestamp - timestamp) / 8.64e7);
+    }
+
     document.body.innerHTML = userData.map((user) => 
         `
             <div class="card">
-                <h3>${user.name.first}</h3>
+                <img src=${user.picture.large} alt="photo de ${user.name.last}">
+                <h3>${user.name.first} ${user.name.last}</h3>
+                <p>${user.location.city}, ${user.location.country}</p>
+                <p>${dateParser(user.dob.date)} </p>
+                <em>Membre depuis : ${daycalc(user.registered.date)} jours </em>
             </div>
 
         `).join("");
