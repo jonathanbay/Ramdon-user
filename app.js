3ce80ae723acbb93351f2ffaa58068a13c6632ac
Jonathn bay <bayart.jonathan@laposte.net>
2023-01-01 15:17:00 +0100

    affichage card

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
 let userData = [];
 
 const fetchUser = async () => {
-    await fetch("https://randomuser.me/api/")
+    await fetch("https://randomuser.me/api/?results=24")
         .then((res) => res.json())
         // .then((data) => console.log(data.results));
         .then((data) => userData = data.results);
     
     console.log(userData);
 }
+const userDisplay =async () => {
+    await fetchUser();
 
-fetchUser();
+    document.body.innerHTML = userData.map((user) => 
+        `
+            <div class="card">
+                <h3>${user.name.first}</h3>
+            </div>
+
+        `).join("");
+}
+
+userDisplay();
